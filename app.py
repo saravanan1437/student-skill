1d0322a4883b935b60ca710f213ed99ce362990d
saravanan1437 <saravananshalu1437@gmail.com>
2026-03-27 09:52:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+import streamlit as st
 
+st.set_page_config(page_title="Career Guidance System")
+
+st.title("🔥 Student Career Guidance System")
+st.write("Welcome nanba 😎")
+
+menu = ["Home", "Login", "Register"]
+
+choice = st.sidebar.selectbox("Menu", menu)
+
+if choice == "Home":
+    st.header("🏠 Home Page")
+    st.write("This is your final year project")
+
+elif choice == "Login":
+    st.header("🔐 Login Page")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        st.success("Login Successful ✅")
+
+elif choice == "Register":
+    st.header("📝 Register Page")
+    new_user = st.text_input("Create Username")
+    new_pass = st.text_input("Create Password", type="password")
+    if st.button("Register"):
+        st.success("Account Created ✅")
